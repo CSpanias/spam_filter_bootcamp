7eae7b6e1d6d96812ecf542b0b7b0e9f63f2ba44
Cspanias <spaniascharalampos@gmail.com>
2021-12-10 23:16:08 +0000

    Added a working second version of a Spam Filter. Changes included: simplifying the code used in the "process_text" function and much more commenting everything for better follow-up from a third person.

--- spam_filter.py
+++ spam_filter.py
@@ -1,8 +1,15 @@
-# Spam Filter Project for AI & ML Bootcamp
+"""
+This code is based on the YouTube tutorial::
+	Email Spam Detection Using Python & Machine Learning:
+	https://www.youtube.com/watch?v=cNLPt02RwF0&list=WL&index=4&t=12s
 
-# Detects if an email is spam (1) or not (0).
+It was modified based on:
+	Sololearn (Machine Learning path):
+	https://www.sololearn.com/learning/1094
 
-# Import libraries
+	Machine Learning for Absolute Beginners:
+	https://www.amazon.co.uk/Machine-Learning-Absolute-Beginners-Introduction-ebook/dp/B08RWBSKQB
+"""
 import pandas as pd
 import string
 
@@ -16,20 +23,26 @@ from sklearn.naive_bayes import MultinomialNB
 df = pd.read_csv(r"C:\Users\10inm\Desktop\spam_filter_bootcamp\emails.csv")
 # print the first 5 rows of data
 print(df.head())
-
 # print the number of rows and columns
 print(df.shape)
 
+"""
+DATA CLEANING PROCESS
+"""
+
 # check for duplicates
 print(df.duplicated().sum())
-
 # remove duplicates
 print(df.drop_duplicates(inplace=True))
-
 # check for missing values
 print(df.isna().sum())
 
-
+"""
+Define a function that:
+	1. Removes punctuation symbols and stopwords from a text
+	2. Splits the text into individual words
+	3. Stores the individual words in a list
+"""
 def process_text(text):
 	"""
 		Returns a list of strings (individual words) of a text (paragraph) excluding
@@ -43,62 +56,79 @@ def process_text(text):
 			-------
 				clean_words: A list of objects (string).
 		"""
-	# remove punctuation
-	no_punctuation = [char for char in text if char not in string.punctuation]
-	# store in no_punctuation list
+	# create an empty list to later store the rows (emails) without punctuation
+	no_punctuation = []
+	# create an empty list to later store the words of each row (email)
+	clean_words = []
+	# for every row (email) in the text column
+	for char in text:
+		# remove every punctuation symbol
+		if char not in string.punctuation:
+			# append the free-of-punctuation row in the list "no_punctuation"
+			no_punctuation.append(char)
+	# insert an empty space between words
 	no_punctuation = ''.join(no_punctuation)
-	# tokenization process (split in words)
-	clean_words = [word for word in no_punctuation.split() if word.lower() not in
-	               stopwords.words('english')]
+
+	# for every row (email without punctuation symbols) in
+	# the "no_punctuation" list split the text into words
+	for word in no_punctuation.split():
+		# convert every word in lowercase
+		word = word.lower()
+		# if this word is not a stopword
+		if word not in stopwords.words('english'):
+			# append the free-of-stopwords words in the list "clean_words"
+			clean_words.append(word)
+	# give back the list "clean_words"
 	return clean_words
 
 
-# apply the function "process_text" to the column "text"
-df['text'].head().apply(process_text)
-
-# convert a collection of text to a matrix of tokens
+# apply the "process_text" function to the column "text"
+# so we end up with a column of individual words
+df['text'].apply(process_text)
+# print the first 5 rows of the column 'text'
+print(df['text'].head())
+# create a matrix of words based on their frequency
 messages_bow = CountVectorizer(analyzer=process_text).fit_transform(df['text'])
+"""
+CountVectorizer transforms a given text into a vector on the basis of 
+the frequency (count) of each word that occurs in the entire text. 
+(*bow = bog of words)
+More info on how it works: 
+https://www.geeksforgeeks.org/using-countvectorizer-to-extracting-features-from-text/
+"""
 
+"""
+MODEL TRAINING PROCESS
+"""
 # assign X and y variables
 X = messages_bow
 y = df['spam']
-
 # split training and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,
                                                     random_state=1)
-
 # get the shape of X (rows, columns)
 print(X.shape)
-
-# create and train the Naive Bayes Classifier
-
-
-# select the model
+# select the model (Naive Bayes Classifier)
 model = MultinomialNB()
-
 # train the model
 model.fit(X_train, y_train)
 
-# check the prediction on the training data
+"""
+EVALUATION PROCESS
+"""
+
+# check the prediction on the training set
 y_pred_train = model.predict(X_train)
-
-# classification report
-print("CLASSIFICATION REPORT\n", classification_report(y_train, y_pred_train))
-
-# confusion matrix
-print("CONFUSION MATRIX\n", confusion_matrix(y_train, y_pred_train))
-
-# accuracy score
-print("ACCURACY SCORE: ", accuracy_score(y_train, y_pred_train))
-
-# use the model to predict on new (test) data
+print('Accuracy Score on Training set: ',
+      round(accuracy_score(y_train, y_pred_train), 4))
+# use the model to predict on the testing set
 y_pred_test = model.predict(X_test)
+print('Accuracy Score on Testing set: ',
+      round(accuracy_score(y_test, y_pred_test), 4))
 
-# classification report
-print("\t\t\tCLASSIFICATION REPORT\n\n", classification_report(y_test, y_pred_test))
+# print the classification report on the testing set
+print("\n\t\t\t\t\tCLASSIFICATION REPORT\n\n",
+      classification_report(y_test, y_pred_test))
 
-# confusion matrix
-print("\nCONFUSION MATRIX\n\n", confusion_matrix(y_test, y_pred_test))
-
-# accuracy score
-print("\nACCURACY SCORE:", accuracy_score(y_test, y_pred_test))
+# print the confusion matrix of the testing set
+print("\nCONFUSION MATRIX\n", confusion_matrix(y_test, y_pred_test))
